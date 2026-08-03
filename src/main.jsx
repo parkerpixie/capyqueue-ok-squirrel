d4b493a3a6ea5496b9b922c3848d66ffb5c2647d
parkerpixie <parkerpixiedust@gmail.com>
2026-08-02 21:19:51 -0500

    Load OK Squirl brand refresh and breathing experience

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './styles.css';
 import './overrides.css';
+import './brand-refresh.css';
+import './brand-enhancements.js';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
