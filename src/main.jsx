ec0f1450ffeb9a1a9f09b0daabf9065d69b08060
parkerpixie <parkerpixiedust@gmail.com>
2026-08-15 02:14:14 -0500

    Wire interactive feelings wheel into DBT Lab

--- src/main.jsx
+++ src/main.jsx
@@ -8,10 +8,12 @@ import './blondie.css';
 import './final-fixes.css';
 import './dbt-lab.css';
 import './dbt-engine.css';
+import './feelings-wheel.css';
 import './brand-enhancements.js';
 import './blondie-enhancements.js';
 import './asset-path-fixes.js';
 import './dbt-lab.js';
+import './feelings-wheel.js';
 import './dbt-engine.js';
 
 createRoot(document.getElementById('root')).render(
